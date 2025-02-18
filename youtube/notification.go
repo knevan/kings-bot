5cd393184512d19decb68a2f3dc700a28e658980
wahyu <wahyuby.nev@gmail.com>
2025-02-18 21:09:21 +0700

    Tidy project structure, use sub folder

---
+++ youtube/notification.go
@@ -0,0 +1,128 @@
+package youtube
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	channelID      string
+	discordChannel string
+	verifyToken    string
+)
+
+// Notification YoutubeNotification struct for xml payload from YouTube
+type Notification struct {
+	XMLName xml.Name `xml:"feed"`
+	Entry   struct {
+		VideoID   string `xml:"videoID"`
+		ChannelID string `xml:"channelID"`
+		Title     string `xml:"title"`
+		Status    struct {
+			Type string `xml:"type,attr"`
+		} `xml:"status"`
+	} `xml:"entry"`
+}
+
+// Init initializes the YouTube module
+func Init(youtubeChannelID, discordChannelID, verifyTokenValue string) {
+	channelID = youtubeChannelID
+	discordChannel = discordChannelID
+	verifyToken = verifyTokenValue
+}
+
+// HandleYoutubeWebhook Handle Webhook
+func HandleYoutubeWebhook(w http.ResponseWriter, r *http.Request, s *discordgo.Session) {
+	log.Printf("Received webhook request: %s %s", r.Method, r.URL.Path)
+
+	// Handle verification GET request
+	if r.Method == "GET" {
+		challenge := r.URL.Query().Get("hub.challenge")
+		verifyTokenRecieved := r.URL.Query().Get("hub.verify_token")
+
+		// log.Printf("Verification attempt - Token: %s, Challenge: %s", verifyToken, challenge)
+		if verifyTokenRecieved != verifyToken {
+			log.Printf("Invalid verify token received: %s", verifyTokenRecieved)
+			http.Error(w, "Invalid verification token.", http.StatusForbidden)
+			return
+		}
+
+		log.Printf("Verification successful, responding with challenge: %s", challenge)
+
+		if _, err := w.Write([]byte(challenge)); err != nil {
+			log.Printf("Error writing challenge response: %v", err)
+			http.Error(w, "Failed to write response", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// Handle notification POST request
+	if r.Method == "POST" {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading body", http.StatusInternalServerError)
+			return
+		}
+		defer func() {
+			if err := r.Body.Close(); err != nil {
+				log.Printf("Error closing body: %v", err)
+			}
+		}()
+
+		var notification Notification
+		if err := xml.Unmarshal(body, &notification); err != nil {
+			http.Error(w, "Error parsing XML youtube notification", http.StatusBadRequest)
+		}
+
+		// Check if channel start Livestream
+		log.Printf("Recieved notification: %v", notification)
+		if notification.Entry.Status.Type == "Live" {
+			message := fmt.Sprintf("\\n%s\\nhttps://youtube.com/watch?v=%s",
+				notification.Entry.Title,
+				notification.Entry.VideoID)
+
+			_, err = s.ChannelMessageSend(discordChannel, message)
+			if err != nil {
+				log.Printf("Error Sending Discord Message: %v", err)
+			} else {
+				log.Printf("%s", notification.Entry.Title)
+			}
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func SubscribeYoutubeChannel(channelID string) error {
+	callbackURL := fmt.Sprintf("https://1673-180-252-122-44.ngrok-free.app/youtube/webhook")
+	topicURL := fmt.Sprintf("https://www.youtube.com/xml/feeds/videos.xml?channel_id=%s", channelID)
+
+	values := url.Values{}
+	values.Set("hub.callback", callbackURL)
+	values.Set("hub.topic", topicURL)
+	values.Set("hub.verify_token", verifyToken)
+	values.Set("hub.mode", "subscribe")
+	values.Set("hub.lease_seconds", "")
+
+	resp, err := http.PostForm("https://pubsubhubbub.appspot.com/subscribe", values)
+	if err != nil {
+		return fmt.Errorf("error subscribing: %v", err)
+	}
+
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Printf("Error closing body: %v", err)
+		}
+	}()
+
+	if resp.StatusCode != http.StatusAccepted && resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("something error in here: %d", resp.StatusCode)
+	}
+	log.Printf("Success subscribe to channel: %s", channelID)
+	return nil
+}
